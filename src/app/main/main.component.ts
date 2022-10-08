a9e945b3eb62be56773521a9f1f94fd72e1daec1
Amin Uddin <aminuddin01697@gmail.com>
2022-10-08 14:00:52 +0530

    map setup completed

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef, OnInit } from 
+  '@angular/core';
 
 @Component({
   selector: 'app-main',
@@ -12,6 +13,7 @@ export class MainComponent implements OnInit {
   data;
   api_key="b326d0250896b8840d30cb1bdd5fcf80";
   baseUrl = "https://api.openweathermap.org/data/2.5/weather?q="
+
   constructor(private http:HttpClient) { }
 
   ngOnInit(): void {
@@ -19,11 +21,43 @@ export class MainComponent implements OnInit {
     this.fetchInfo();
   }
 
+   @ViewChild('mapContainer', { static: false }) gmap: ElementRef;
+    map: google.maps.Map;
+    lat = 0;
+    lng = 0;
+
+    coordinates = new google.maps.LatLng(this.lat, this.lng);
+
+    mapOptions: google.maps.MapOptions = {
+     center: this.coordinates,
+     zoom: 8
+    };
+
+    marker = new google.maps.Marker({
+      position: this.coordinates,
+    });
+
+    ngAfterViewInit() {
+      this.mapInitializer();
+    }
+
+    mapInitializer() {
+      this.map = new google.maps.Map(this.gmap.nativeElement, 
+      this.mapOptions);
+      this.marker.setMap(this.map);
+    }
+  
+
+
+
+ 
   fetchInfo() {
    const response = this.http.get(`${this.baseUrl}${this.search}&appid=${this.api_key}`);
    response.subscribe(
      (res) => {
        this.data = res;
+       this.lat = res['coord']['lat']
+       this.lng = res['coord']['lon']
        this.cityName =  this.search
        this.search = ""
      }, 
